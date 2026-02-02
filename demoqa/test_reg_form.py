8765d7edc2fa3c5e3d6401681c53f6542f04e5cc
Artem Shmakov <artem.shm.2003@mail.ru>
2026-02-02 13:49:20 +0300

    test with selenoid added

---
+++ demoqa/test_reg_form.py
@@ -0,0 +1,58 @@
+
+import allure
+from selene import browser, by, have, command
+
+
+@allure.title("Заполнение регистрационной формы DemoQA")
+def test_form_fill():
+    with allure.step("Заполнить все поля формы"):
+        browser.open('https://demoqa.com/automation-practice-form')
+
+        # Персональные данные
+        browser.element('#firstName').type('Artem')
+        browser.element('#lastName').type('Shmakov')
+        browser.element('#userEmail').type('A.Shmakov@mail.ru')
+        browser.element(by.text('Male')).click()
+        browser.element('#userNumber').type('1234567890')
+
+        # Дата рождения
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__year-select').click().element(by.text('2003')).click()
+        browser.element('.react-datepicker__month-select').click().element(by.text('January')).click()
+        browser.element('.react-datepicker__day--028').click()
+
+        # Предметы и хобби
+        browser.element('#subjectsInput').type('ma').press_enter()
+        browser.element('#submit').perform(command.js.scroll_into_view)
+        browser.element('label[for="hobbies-checkbox-1"]').click()
+
+        # Адрес
+        browser.element('#currentAddress').type('Улица колотушкина 10')
+
+        # Штат и город
+        browser.element('#state').click()
+        browser.element('#react-select-3-input').type('nc').press_enter()
+        browser.element('#city').click()
+        browser.element('#react-select-4-input').type('d').press_enter()
+
+        # Отправить
+        browser.element('#submit').click()
+
+
+
+
+    with allure.step("Проверить что данные отобразились корректно"):
+        browser.all('.modal-body tr td:last-child').should(have.texts(
+            'Artem Shmakov',
+            'A.Shmakov@mail.ru',
+            'Male',
+            '1234567890',
+            '28 January,2003',
+            'Maths',
+            'Sports',
+            '',
+            'Улица колотушкина 10',
+            'NCR Delhi'
+        ))
+
+
