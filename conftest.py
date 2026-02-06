4b3b2a8b6f56a7d912abb4ec80a52bc3b283430f
Artem Shmakov <artem.shm.2003@mail.ru>
2026-02-06 15:47:30 +0300

    attaches reworked

--- conftest.py
+++ conftest.py
@@ -8,13 +8,20 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_version",
+        default="127.0"
+    )
+
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_version = request.config.getoption("--browser_version")
     options = Options()
 
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "127.0")
+    options.set_capability("browserName", 'chrome')
+    options.set_capability("browserVersion", browser_version)
     options.set_capability("selenoid:options", {
         "enableVNC": True,
         "enableVideo": True
